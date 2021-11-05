1532fb1c384bbffefecfb31ad9c877b61b550bbe
Jschles1 <jschles1@ramapo.edu>
2021-11-05 17:26:53 -0400

    Add test for rinkeby, refactor provider and contract connection

--- src/App.js
+++ src/App.js
@@ -9,18 +9,50 @@ const contractABI = abi.abi;
 
 const App = () => {
     const [currentAccount, setCurrentAccount] = useState('');
+    const [network, setNetwork] = useState('');
+    const [contract, setContract] = useState('');
     const [allWaves, setAllWaves] = useState([]);
     const { register, handleSubmit } = useForm();
+
+    const isRinkeby = network === 'rinkeby';
+
+    const getWeb3Provider = () => {
+        const { ethereum } = window;
+        const provider = new ethers.providers.Web3Provider(ethereum, 'any');
+
+        provider.on('network', (newNetwork, oldNetwork) => {
+            // When a Provider makes its initial connection, it emits a "network"
+            // event with a null oldNetwork along with the newNetwork. So, if the
+            // oldNetwork exists, it represents a changing network
+            if (oldNetwork) {
+                provider.removeAllListeners();
+                window.location.reload();
+            } else {
+                console.log('network', newNetwork.name);
+                setNetwork(newNetwork.name);
+            }
+        });
+
+        ethereum.on('accountsChanged', async () => {
+            const accounts = await provider.listAccounts();
+            setCurrentAccount(accounts[0] ? accounts[0] : '');
+        });
+
+        return provider;
+    };
+
+    const getWavePortalContract = () => {
+        const provider = getWeb3Provider();
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+        return wavePortalContract;
+    };
 
     const getAllWaves = async () => {
         try {
             const { ethereum } = window;
-            if (ethereum) {
-                const provider = new ethers.providers.Web3Provider(ethereum);
-                const signer = provider.getSigner();
-                const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-
-                const waves = await wavePortalContract.getAllWaves();
+            if (ethereum && contract) {
+                const waves = await contract.getAllWaves();
 
                 let wavesCleaned = [];
                 waves.forEach((wave) => {
@@ -32,8 +64,6 @@ const App = () => {
                 });
 
                 setAllWaves(wavesCleaned);
-            } else {
-                console.log("Ethereum object doesn't exist!");
             }
         } catch (error) {
             console.log(error);
@@ -57,7 +87,9 @@ const App = () => {
                 const account = accounts[0];
                 console.log('Found an authorized account:', account);
                 setCurrentAccount(account);
-                getAllWaves();
+                if (!contract) {
+                    setContract(getWavePortalContract());
+                }
             } else {
                 console.log('No authorized account found');
             }
@@ -79,7 +111,9 @@ const App = () => {
 
             console.log('Connected', accounts[0]);
             setCurrentAccount(accounts[0]);
-            getAllWaves();
+            if (!contract) {
+                setContract(getWavePortalContract());
+            }
         } catch (error) {
             console.log(error);
         }
@@ -92,20 +126,16 @@ const App = () => {
                 const { ethereum } = window;
 
                 if (ethereum) {
-                    const provider = new ethers.providers.Web3Provider(ethereum);
-                    const signer = provider.getSigner();
-                    const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
-
-                    let count = await wavePortalContract.getTotalWaves();
+                    let count = await contract.getTotalWaves();
                     console.log('Retrieved total wave count...', count.toNumber());
 
-                    const waveTxn = await wavePortalContract.wave(message, { gasLimit: 300000 });
+                    const waveTxn = await contract.wave(message, { gasLimit: 300000 });
                     console.log('Mining...', waveTxn.hash);
 
                     await waveTxn.wait();
                     console.log('Mined -- ', waveTxn.hash);
 
-                    count = await wavePortalContract.getTotalWaves();
+                    count = await contract.getTotalWaves();
                     console.log('Retrieved total wave count...', count.toNumber());
                 } else {
                     console.log("Ethereum object doesn't exist!");
@@ -119,6 +149,18 @@ const App = () => {
     useEffect(() => {
         checkIfWalletIsConnected();
     }, []);
+
+    useEffect(() => {
+        if (contract) {
+            getAllWaves();
+
+            contract.on('NewWave', getAllWaves);
+
+            return () => {
+                contract.off('NewWave');
+            };
+        }
+    }, [contract]);
 
     return (
         <div className="mainContainer">
@@ -135,9 +177,29 @@ const App = () => {
                 {!!currentAccount && (
                     <>
                         <textarea {...register('message')} name="message" />
-                        <button className="waveButton" onClick={handleSubmit(wave)}>
+                        <button className="waveButton" onClick={handleSubmit(wave)} disabled={!isRinkeby}>
                             Wave at Me
                         </button>
+                        {isRinkeby ? (
+                            allWaves.map((wave, index) => {
+                                return (
+                                    <div
+                                        key={index}
+                                        style={{ backgroundColor: 'OldLace', marginTop: '16px', padding: '8px' }}
+                                    >
+                                        <div>Address: {wave.address}</div>
+                                        <div>Time: {wave.timestamp.toString()}</div>
+                                        <div>Message: {wave.message}</div>
+                                    </div>
+                                );
+                            })
+                        ) : (
+                            <p style={{ textAlign: 'center', color: 'red', lineHeight: '30px' }}>
+                                You must be connected to the Rinkeby network to use this application.
+                                <br /> Please switch to the Rinkeby network.
+                            </p>
+                        )}
+                        {}
                     </>
                 )}
 
@@ -146,16 +208,6 @@ const App = () => {
                         Connect Wallet
                     </button>
                 )}
-
-                {allWaves.map((wave, index) => {
-                    return (
-                        <div key={index} style={{ backgroundColor: 'OldLace', marginTop: '16px', padding: '8px' }}>
-                            <div>Address: {wave.address}</div>
-                            <div>Time: {wave.timestamp.toString()}</div>
-                            <div>Message: {wave.message}</div>
-                        </div>
-                    );
-                })}
             </div>
         </div>
     );
