95dc3439d06026cf45080ca32b3aa175f558d38d
Jschles1 <johnschlesinger5@gmail.com>
2021-11-18 19:48:01 -0500

    Add chakra theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import theme from '@chakra-ui/theme';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const customTheme = extendTheme({
+    ...theme,
+    styles: {
+        ...theme.styles,
+        global: (props) => ({
+            ...theme.styles.global,
+            body: {
+                ...theme.styles.global.body,
+                bg: '#0d1116',
+                color: 'white',
+                overflowX: 'hidden',
+            },
+        }),
+    },
+    colors: {
+        main: {
+            100: '#0d1116',
+            200: '#121f2f',
+            300: '#394a60',
+        },
+    },
+});
+
+ReactDOM.render(
+    <ChakraProvider theme={customTheme}>
+        <App />
+    </ChakraProvider>,
+    document.getElementById('root')
+);
