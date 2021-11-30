2a15e6b85d53100e4512cdfb7788796c5f843e5a
Jschles1 <johnschlesinger5@gmail.com>
2021-11-29 21:05:54 -0500

    Revert "Restructure app layout and add Menu"
    This reverts commit 8380efb3398b78fe05edfaf520b6f512c8864400.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box } from '@chakra-ui/layout';
 
 const Layout = ({ children }) => (
-    <Box maxW="1440px" p={4} mx="auto" bgColor="main.100" flex="1">
+    <Box maxW="1440px" p={4} mx="auto" bgColor="main.100">
         {children}
     </Box>
 );
